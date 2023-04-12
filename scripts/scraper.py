f503887f9c878f69e942df5c2d5c79ffcf08d65e
Rudolf Nosek <rudolfnosek.rn@gmail.com>
2023-04-12 18:27:15 +0200

    feat(scaper): region scraping added

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -44,6 +44,50 @@ def sister_cities_philippines() -> dict:
 def sister_cities_for_regions(link:str) -> dict:
     data = {} # {city -> {sister_city -> [sister_cities], country_of_sc -> [country_of_sc]},
               #  country -> country}
+    page = requests.get(link)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    states = soup.find_all("h2", class_="section-heading")
+
+    states_names = []
+    for state in states:
+        state = state.text[:-4]
+        if state != 'References':
+            states_names.append(state)
+    
+    for index,state in enumerate(states_names):
+        result = soup.find_all("section", class_="mf-section-"+str(index+1))
+        for section in result:
+            towns = []
+            for town in section.find_all('p'):
+                town = remove_brackets(town.text.strip())
+                data[town] = {}
+                towns.append(town)
+   
+            for index2,list_sc in enumerate(section.find_all('ul')):
+
+                try:
+                    data[towns[index2]]['sister_city'] = []
+                    data[towns[index2]]['country_of_sc'] = []
+                except IndexError:
+                    print(index2)
+                    break
+                
+                for sc in list_sc.find_all('li'):
+                    
+                    l = list(map(remove_brackets,sc.text.strip().split(',')))
+                    if len(l) == 1:
+                        city = l[0]
+                        country = "NaN"
+                    else:
+                        city = l[0]
+                        country = l[-1]
+
+                    data[towns[index2]]['sister_city'].append(city)
+                    data[towns[index2]]['country_of_sc'].append(country)
+                    data[towns[index2]]['country'] = state
+    return data
 
 def sister_cities(link:str) -> dict:
     data = {} # city -> {sister_city -> [sister_cities], country_of_sc -> [country_of_sc]}
@@ -94,7 +138,6 @@ def sister_cities(link:str) -> dict:
         
 
 if __name__ == "__main__":
-    # add links to certain states from US
     data = {}
     links = links_from_mainpage()
     
@@ -122,6 +165,22 @@ if __name__ == "__main__":
     special_us_states_links = ["https://en.m.wikipedia.org/wiki/List_of_sister_cities_in_Maryland",
                                "https://en.m.wikipedia.org/wiki/List_of_sister_cities_in_Florida"]
 
+    for link in links_for_regions:
+        region = link
+        while region.find('in_') != -1:
+            region = region[region.find('in_')+3:]
+        
+        print(region)
+        sc = sister_cities_for_regions(link)
+        print()
+
+        for town in sc:
+            sc[town]['continent'] = region
+            if region == 'New_England':
+                sc[town]['country'] = 'the_United_States'
+                sc[town]['continent'] = "North America"
+        data.update(sc)
+
     for link, continent in links:
         country = link
         while country.find('in_') != -1:
@@ -145,4 +204,20 @@ if __name__ == "__main__":
             sc[town]['country'] = 'the_United_States'
             sc[town]['continent'] = 'North America'
         data.update(sc)
-print(data)
+
+    # Adding thailand
+    data['Bangkok'] = {'sister_city':["Aichi Prefecture","Ankara","Astana","Beijing","Chaozhou","Chonqing",'Fukuoka Prefecture','Guangzhou','Hanoi','Ho Chi Minh City','Istanbul','Jakarta','Lausanne','Manila','Moscow','Penang Island','Phnom Penh','Saint Petersburg','Seoul','Shandong','Shanghai','Ulaanbaatar','Washington, D.C.','Wuhan'],
+                    'country_of_sc':['Japan',"Turkey","Kazakhstan",'China','China','China','Japan','China','Vietnam','Vietnam','Turkey','Indonesia','Switzerland','Philippines','Russia','Malaysia','Cambodia','Russia','South Korea','China','China','Mongolia','United States','China'],
+                    'country':'Thailand',
+                    'continent':'Asia'}
+    data['Chiang Rai'] = {'sister_city':["Union City"],
+                        'country_of_sc':['United States'],
+                        'country':'Thailand',
+                        'continent':'Asia'}
+    data['Udon Thani'] = {'sister_city':["Reno"],
+                        'country_of_sc':['United States'],
+                        'country':'Thailand',
+                        'continent':'Asia'}
+
+
+    print(data)
